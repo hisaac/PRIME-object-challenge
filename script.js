1521e233a557158133f3164e1705c27d701d26cb
Isaac Halvorson <hello@hisaac.net>
2016-10-26 20:45:55 -0500

    finish javascript and tweak styling

--- script.js
+++ script.js
@@ -5,36 +5,30 @@ function State(state, revenue, locationCount){
   this.state = state;
   this.revenue = revenue;
   this.locationCount = locationCount;
-  function averageRevenue(){
-    return this.this.revenue / this.this.locationCount;
+  this.averageRevenue = function(){
+    var avg = revenue / locationCount;
+    return avg;
   }
-  averageRevenue();
 }
 
 // object instances
 var illinois = new State("il", 5000, 12);
-console.log(illinois.state);
-
 var minnesota = new State("mn", 300, 3);
-console.log(minnesota.state);
 var nevada = new State("nv", 25000, 1);
-console.log(nevada.state);
 
 // push object instances to locations array
 locations.push(illinois);
 locations.push(minnesota);
 locations.push(nevada);
 
-console.log(locations);
-
 // DO NOT MODIFY THIS CODE
 $(document).ready(function () {
   locations.forEach(function (element, index) {
     var $li = $('<li></li>');
-    $li.append('<span>' +  element.state + '</span>');
-    $li.append('<span>' +  element.revenue + '</span>');
-    $li.append('<span>' +  element.locationCount + '</span>');
-    $li.append('<span>' +  element.averageRevenue() + '</span>');
+    $li.append('<span><strong>State: </strong>' + element.state.toUpperCase() + ' |</span>');
+    $li.append('<span><strong>Total revenue: </strong>' + element.revenue + ' |</span>');
+    $li.append('<span><strong>Total locations: </strong>' + element.locationCount + ' |</span>');
+    $li.append('<span><strong>Average revenue: </strong>' + element.averageRevenue() + '</span>');
     $('ol').append($li);
   });
 });
